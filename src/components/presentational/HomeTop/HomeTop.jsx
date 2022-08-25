3510a5d266e1ef6ff3e7d25d3dc7c69bbbba9ad5
Dalton Steele <dsteele92@gmail.com>
2022-08-25 14:45:15 -0500

    Polished nav bar transitions, added down arrow that is lightly changing opacity.

--- src/components/presentational/HomeTop/HomeTop.jsx
+++ src/components/presentational/HomeTop/HomeTop.jsx
@@ -1,16 +1,15 @@
 import { React, useState, useEffect, useRef } from 'react';
-
 import Style from './homeTop.module.scss';
+import { BsArrowDownCircle } from 'react-icons/bs';
 
 export default function HomeTop() {
 	const header = useRef();
-	const [yScroll, setYScroll] = useState(0);
+	// const [yScroll, setYScroll] = useState(0);
 
 	useEffect(() => {
 		const handleScroll = (event) => {
-			setYScroll(window.scrollY);
-			// console.log(window.scrollY);
-			console.log(header.current.style);
+			console.log(window.scrollY);
+			// console.log(header.current.style);
 			header.current.style.fontSize = `${40 + window.scrollY / 10}px`;
 			header.current.style.opacity = `${1 / (window.scrollY / 100)}`;
 		};
@@ -21,12 +20,22 @@ export default function HomeTop() {
 		};
 	}, []);
 
+	// const downArrowClick = function () {
+	// 	console.log('clicked');
+	// 	const vh = window.outerHeight;
+	// 	console.log(vh);
+	// 	window.scrollTo({ top: vh, behavior: 'smooth' });
+	// 	// window.scrollY = 1000;
+	// };
+
 	return (
 		<div className={Style.Top}>
 			<div className={Style.BackgroundOne}>
 				<div className={Style.Overlay}>
 					<div className={Style.Content}>
 						<h1 ref={header}>Life Improvements</h1>
+						<BsArrowDownCircle className={Style.Arrow} />
+						{/* <p>Scroll Down</p> */}
 					</div>
 				</div>
 			</div>
